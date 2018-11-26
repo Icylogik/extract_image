b54664182e31e40c562e72f8ee9f4d7301e1d043
Icylogik <Icylogik@users.noreply.github.com>
2018-11-26 22:40:17 +0700

    Create extract_image.py

---
+++ extract_image.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+have_PIL = True
+try:
+    import PIL.Image
+except ImportError:
+    have_PIL = False
+
+have_numpy = True
+try:
+    import numpy as np
+except ImportError:
+    have_numpy = False
+
+
+def get_LUT_value(data, window, level):
+    """Apply the RGB Look-Up Table for the given
+       data and window/level value."""
+    if not have_numpy:
+        raise ImportError("Numpy is not available."
+                          "See http://numpy.scipy.org/"
+                          "to download and install")
+    try:
+        window = window[0]
+    except TypeError:
+        pass
+    try:
+        level = level[0]
+    except TypeError:
+        pass
+    
+    return np.piecewise(data,
+                        [data <= (level - 0.5 - (window - 1) / 2),
+                         data > (level - 0.5 + (window - 1) / 2)],
+                        [0, 255, lambda data: ((data - (level - 0.5)) /
+                         (window - 1) + 0.5) * (255 - 0)])
+                         
+                         
+
+def linear_transform(dicom_data):
+    """Linearly rescale the dicom image using the 
+       RescaleIntercept and RescaleSlope values 
+       extracted from the given DICOM data."""
+    im_int16 = dicom_data.pixel_array.astype(np.int16)
+    im_int16[im_int16== -2000] = 0
+    intercept = dicom_data.RescaleIntercept
+    slope = dicom_data.RescaleSlope
+    if slope != 1:
+            im_int16 = slope * im_int16.astype(np.float64)
+            im_int16 = im_int16.astype(np.int16)
+
+    transformed_pic =   im_int16 + np.int16(intercept)
+    
+    return transformed_pic
+    
+
+                         
+def extract(dicom_data):
+    """Extract viewable image and the slice location 
+       from the given dicom data."""
+    image_data = pdc.dcmread(dicom_data)
+    pic = linear_transform(image_data)
+    im_transformed = get_LUT_value(pic, image_data.WindowWidth,
+                                image_data.WindowCenter)
+    slice_loc = dataset.SliceLocation
+    
+    return im_transformed, slice_loc
+                                
